6fb330d32752825bd318932c5c355783bbc58cc2
Duude92 <switch4acc@gmail.com>
2025-05-13 11:52:00 +0300

    Added check for user to be exist in database Added registration for non-existent user Added answer

--- src/server/commands/playerReg.ts
+++ src/server/commands/playerReg.ts
@@ -1,8 +1,31 @@
 import { IRoutedCommand } from '../../api/IRoutedCommand';
-import { IRequestRegister } from './IRegistration';
+import { IRequestRegister, IResponseRegister } from './IRegistration';
+import { dbContext } from '../../memoryDbProvider/dbProvider';
+import { randomUUID } from 'node:crypto';
+import { createCommandObject } from '../../api/ICommand';
 
 const register = async (payload: IRequestRegister, socket: WebSocket) => {
-    console.log(payload);
+    let existingUser = dbContext.users.find(
+        (user) => user.name === payload.name
+    );
+    if (!existingUser) {
+        const newId = randomUUID();
+        existingUser = {
+            name: payload.name,
+            password: payload.password,
+            id: newId
+        };
+        dbContext.users.push(existingUser);
+
+    }
+    const response: IResponseRegister = {
+        name: existingUser!.name,
+        index: existingUser!.id,
+        error: false,
+        errorText: ''
+    };
+    const answer = createCommandObject('reg', response);
+    socket.send(JSON.stringify(answer));
 };
 
 export const createCommand = (): IRoutedCommand => ({
