9ab472e3c60fa47d9d9df7d534dfad93c2ead7ab
Duude92 <switch4acc@gmail.com>
2025-05-14 17:53:02 +0300

    Implemented function to get all occupied cells by ship

--- src/server/commands/api/IShipsData.ts
+++ src/server/commands/api/IShipsData.ts
@@ -7,9 +7,33 @@ export interface IShipsData {
     ships: IShip[];
 }
 
-interface IShip {
-    position: { x: number; y: number };
+export interface IShip {
+    position: vector2d;
     direction: boolean;
     length: number;
-    type: 'small' | 'medium' | 'large' | 'huge';
+    type: ShipSize;
 }
+
+interface vector2d {
+    x: number;
+    y: number;
+}
+
+enum ShipSize {
+    'small' = 0,
+    'medium',
+    'large',
+    'huge'
+}
+
+export const getShipCells = (ship: IShip): vector2d[] => {
+    const pivot = ship.position;
+    const cells: vector2d[] = [pivot];
+    for (let i = 0; i < ship.type; i++) {
+        cells.push({
+            x: pivot.x + Number(ship.direction),
+            y: pivot.y + Number(!ship.direction)
+        });
+    }
+    return cells;
+};
