6de50d2a190de09bb22aa07a03d576a22424815d
Duude92 <switch4acc@gmail.com>
2025-05-14 09:50:15 +0300

    Fix Session class, provided addShips method, added turn field

--- src/sessionProvider/session.ts
+++ src/sessionProvider/session.ts
@@ -1,6 +1,22 @@
+import { IShipsData } from '../server/commands/api/IShipsData';
+
+enum Turn {
+    first = 0,
+    second
+}
+
 export class Session {
     public readonly gameId: string;
+    private readonly boards: IShipsData[];
+    private turn: Turn;
+
     constructor(gameId: string) {
         this.gameId = gameId;
+        this.boards = [];
+        this.turn = Turn.first;
+    }
+
+    addShips(data: IShipsData) {
+        this.boards.push(data);
     }
 }
