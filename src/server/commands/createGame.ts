01870e13856728d3977672e53ce3c9bbaa32027e
Duude92 <switch4acc@gmail.com>
2025-05-14 09:08:30 +0300

    Fix createGame to create new session from room, destroy room

--- src/server/commands/createGame.ts
+++ src/server/commands/createGame.ts
@@ -1,16 +1,25 @@
 import { createCommandObject, ICommand } from '../../api/ICommand';
 import { roomProvider } from '../../roomProvider/roomProvider';
 import { connectionProvider } from '../server';
+import { sessionProvider } from '../../sessionProvider/sessionProvider';
+import { Session } from '../../sessionProvider/session';
 
 export const createGame = async (roomId: string | number) => {
     const room = roomProvider.rooms.find((room) => room.roomId === roomId);
+    if (!room) {
+        throw new Error('Room not found');
+    }
+    const session = new Session(room.roomId.toString());
+    sessionProvider.sessions.push(session);
     const command = createCommandObject('create_game', {
         idGame: roomId,
-        idPlayer: room!.roomUsers.map((user) => user.user.id)
+        idPlayer: room.roomUsers.map((user) => user.user.id)
     });
-    room!.roomUsers.forEach((user) => {
+    room.roomUsers.forEach((user) => {
         connectionProvider.connections
             .find((conn) => conn.userId == user.user.id)
             ?.socket.send(JSON.stringify(command));
     });
+    const roomIndex = roomProvider.rooms.findIndex((rm) => rm == room);
+    roomProvider.rooms.splice(roomIndex, 1);
 };
