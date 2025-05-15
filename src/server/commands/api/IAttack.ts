d77476e1a68e24e97946bd700404f96cb7ecf18d
Duude92 <switch4acc@gmail.com>
2025-05-15 16:45:33 +0300

    Added typeguard for IAttackResponse

--- src/server/commands/api/IAttack.ts
+++ src/server/commands/api/IAttack.ts
@@ -3,18 +3,28 @@ import { UserIdType } from '../../../api/storage/IUser';
 
 export type AttackResult = 'miss' | 'killed' | 'shot' | 'unprocessed';
 
+export const isAttackRequest = (object: any): object is IAttackRequest => {
+    if (typeof object !== 'object') return false;
+    return (
+        'gameId' in object &&
+        'x' in object &&
+        'y' in object &&
+        'indexPlayer' in object
+    );
+};
+
 export interface IAttackRequest {
     gameId: RoomIdType;
     x: number;
     y: number;
     indexPlayer: UserIdType;
 }
-export interface IAttackResponse{
-    position:
-        {
-            x: number;
-            y: number
-        };
+
+export interface IAttackResponse {
+    position: {
+        x: number;
+        y: number;
+    };
     currentPlayer: UserIdType;
-    status: AttackResult,
+    status: AttackResult;
 }
