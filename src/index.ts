8d52d940dec0e8269dea967647eaad094c6f6dc8
Duude92 <switch4acc@gmail.com>
2025-05-16 11:05:40 +0300

    Setup logger

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import { configDotenv } from 'dotenv';
 import { startServer } from './server/server';
+import { logger } from './logger/logger';
 
 configDotenv();
+logger.setup(process.stdout);
 
 const serverPort = Number(process.env.SERVER_PORT) || 3000;
 
