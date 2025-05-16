061afb466ec7e4dc9971fac3ce83fe8605994a60
Duude92 <switch4acc@gmail.com>
2025-05-16 18:25:05 +0300

    Implementation of router

--- src/bot/botCommandRouter.ts
+++ src/bot/botCommandRouter.ts
@@ -1,12 +1,28 @@
 import { getCommands } from '../utility/commandLoader';
-import { IRoutedCommand } from '../api/IRoutedCommand';
+import { IBotRoutedCommand } from '../api/IRoutedCommand';
+import { IBot } from './botClient';
+import { ICommand } from '../api/ICommand';
+import { logger, MESSAGE_TYPE } from '../logger/logger';
 
 getCommands('./bot/commands/').then((loadedCommands) =>
-    commands.push(...loadedCommands.filter((cmd) => !!cmd))
+    commands.push(
+        ...loadedCommands
+            .filter((cmd) => !!cmd)
+            .map((cmd) => cmd as unknown as IBotRoutedCommand)
+    )
 );
-const commands: IRoutedCommand[] = [];
+const commands: IBotRoutedCommand[] = [];
 
-export const routeMessage = async (
-    incomingMessage: string,
-    socket: WebSocket
-) => {};
+export const routeMessage = async (incomingMessage: string, socket: IBot) => {
+    const request = JSON.parse(incomingMessage) as ICommand;
+    const routedCommand = commands.find((cmd) => cmd.route === request.type);
+    if (!routedCommand) {
+        throw new Error(`${request.type} command not found`);
+    }
+    const command = routedCommand?.command;
+    logger.log(MESSAGE_TYPE.REQUEST, request);
+
+    // const result =
+    await command(request.data, socket);
+    // result.forEach((res) => connectionProvider.unicast(userId, res));
+};
