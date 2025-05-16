3b44f7a55bb2fbe60af35c607a1e3d1a4f35afab
Duude92 <switch4acc@gmail.com>
2025-05-16 18:12:11 +0300

    Added IBotRoutedCommand that would provide IBot

--- src/bot/botClient.ts
+++ src/bot/botClient.ts
@@ -2,11 +2,20 @@ import ws from 'ws';
 import { routeMessage } from './botCommandRouter';
 import { UserIdType } from '../api/storage/IUser';
 
+export interface IBot {
+    client: ws;
+    sessionId: Promise<UserIdType>;
+    response: (message: string) => void;
+    resolveSessionId: (id: UserIdType) => void;
+}
 export const createBot = () => {
     const client = new ws.WebSocket('ws://localhost:3000');
     const bot = {
+    const bot: IBot = {
         client: client,
-        sessionId: <UserIdType | null>null
+        response: (message) => client.send(message),
+        sessionId: resolvablePromise.promise,
+        resolveSessionId: resolvablePromise.resolve
     };
     client.on('open', (socket: WebSocket) => {
         socket.onmessage = async (message) => {
