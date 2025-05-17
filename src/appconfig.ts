7dbf627cc85a0aff5256c7becc614f9aedfb02b9
Duude92 <switch4acc@gmail.com>
2025-05-17 10:28:07 +0300

    Added appconfig to parse environment variables

---
+++ src/appconfig.ts
@@ -0,0 +1,10 @@
+import { configDotenv } from 'dotenv';
+
+configDotenv();
+
+const isTrue = (value: string | undefined): boolean =>
+    value?.toLowerCase() === 'true';
+
+export const SERVER_PORT = Number(process.env.SERVER_PORT) || 3000;
+export const BOT_LOG = isTrue(process.env.BOT_LOGS) || false;
+console.log('ab');
