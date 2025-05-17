40a4b5fe5667804a2da2f8e88f74517cfdf24758
Duude92 <switch4acc@gmail.com>
2025-05-17 12:06:29 +0300

    Fix eslint errors

--- src/server/commands/api/IAttack.ts
+++ src/server/commands/api/IAttack.ts
@@ -3,8 +3,9 @@ import { UserIdType } from '../../../api/storage/IUser';
 
 export type AttackResult = 'miss' | 'killed' | 'shot' | 'unprocessed';
 
-export const isAttackRequest = (object: any): object is IAttackRequest => {
+export const isAttackRequest = (object: unknown): object is IAttackRequest => {
     if (typeof object !== 'object') return false;
+    if (object === null) return false;
     return (
         'gameId' in object &&
         'x' in object &&
--- src/server/commands/randomAttack.ts
+++ src/server/commands/randomAttack.ts
@@ -2,9 +2,6 @@ import { IRoutedCommand } from '../../api/IRoutedCommand';
 import { UserIdType } from '../../api/storage/IUser';
 import { RoomIdType } from '../../api/IRoom';
 import { sessionProvider } from '../../sessionProvider/sessionProvider';
-import { connectionProvider } from '../server';
-import { createCommandObject } from '../../api/ICommand';
-import { turn } from './turn';
 import { attack } from './attack';
 
 const randomAttack = async (payload: string, userId: UserIdType) => {
