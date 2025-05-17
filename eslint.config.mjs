6d7da8563df95c7ccc14c6d2fa45118e3667bf72
Duude92 <switch4acc@gmail.com>
2025-05-17 17:35:01 +0300

    Added webpack.config.js

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,7 +3,7 @@ import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
     {
-        ignores: ['websockets-ui/**']
+        ignores: ['websockets-ui/**', 'webpack.config.js']
     },
     eslint.configs.recommended,
     tseslint.configs.recommended
