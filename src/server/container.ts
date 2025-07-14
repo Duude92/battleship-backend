7675d7bc47a9c4b496b3e06f2a5bb1128c1d19e8
Duude92 <switch4acc@gmail.com>
2025-07-14 11:09:10 +0300

    Added container wrapper

---
+++ src/server/container.ts
@@ -0,0 +1,17 @@
+import { ContainerFactory } from '@duude92/lazyinject';
+import { Container } from '@duude92/lazyinject/dist/container';
+import { InterfaceType } from '@duude92/lazyinject/dist/api/interfaceType';
+
+let containerInternal: Container;
+
+export const container = {
+    async getMany<T>(type: InterfaceType) {
+        if (!containerInternal) {
+            containerInternal = await ContainerFactory.create({
+                baseDir: __dirname,
+                catalogs: ['./commands/']
+            });
+        }
+        return containerInternal.getMany<T>(type);
+    }
+};
