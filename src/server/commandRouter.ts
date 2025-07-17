71ea8b9b66f3addf2f171b580f628440b39c2de8
Duude92 <switch4acc@gmail.com>
2025-07-17 12:33:23 +0300

    CommandRouter as a class, import commands as constructor parameter

--- src/server/commandRouter.ts
+++ src/server/commandRouter.ts
@@ -2,29 +2,33 @@ import { IRoutedCommand } from '../api/IRoutedCommand';
 import { ICommand } from '../api/ICommand';
 import { connectionProvider } from './server';
 import { logger, MESSAGE_TYPE } from '../logger/logger';
-import { container } from './container';
+import { Export, ImportMany } from '@duude92/lazyinject';
 
-const commands: IRoutedCommand[] = [];
-(async () => {
-    const newcommands =
-        await container.getMany<IRoutedCommand>('IRoutedCommand').then(result => commands.concat(result!))
-    commands.push(...newcommands!);
-})();
-export const routeMessage = async (
-    incomingMessage: string,
-    socket: WebSocket
-) => {
-    const request = JSON.parse(incomingMessage) as ICommand;
-    const routedCommand = commands.find((cmd) => cmd.route === request.type);
-    if (!routedCommand) {
-        throw new Error(`${request.type} command not found`);
-    }
-    const command = routedCommand?.command;
-    logger.log(MESSAGE_TYPE.REQUEST, request);
-    const userId = connectionProvider.connections.find(
-        (conn) => conn.socket === socket
-    )!.userId;
+@Export('CommandRouter')
+export class CommandRouter {
+    constructor(
+        @ImportMany('IRoutedCommand')
+        private readonly commands: IRoutedCommand[]
+    ) {}
 
-    const result = await command(request.data, userId);
-    result.forEach((res) => connectionProvider.unicast(userId, res));
-};
+    public routeMessage = async (
+        incomingMessage: string,
+        socket: WebSocket
+    ) => {
+        const request = JSON.parse(incomingMessage) as ICommand;
+        const routedCommand = this.commands.find(
+            (cmd) => cmd.route === request.type
+        );
+        if (!routedCommand) {
+            throw new Error(`${request.type} command not found`);
+        }
+        const command = routedCommand?.command;
+        logger.log(MESSAGE_TYPE.REQUEST, request);
+        const userId = connectionProvider.connections.find(
+            (conn) => conn.socket === socket
+        )!.userId;
+
+        const result = await command(request.data, userId);
+        result.forEach((res) => connectionProvider.unicast(userId, res));
+    };
+}
